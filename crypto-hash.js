43e41ed5f1088ac7bba0b9664a4fff2d98c150dd
Iman <sharifiiman@gmail.com>
2022-09-05 12:16:00 +0430

    Minor Changes Applied

---
+++ crypto-hash.js
@@ -0,0 +1,16 @@
+// const edge = require("edge-js");
+const sha256 = require("fast-sha256");
+
+var java = require("java");
+var jarfile = "BlockChain\\BlockChain.jar";
+java.classpath.push(jarfile);
+
+const cryptoHash = (...inputs) => {
+  var str_in = inputs.sort().join(" ");
+  return java.callStaticMethodSync("Simple", "SHA256", str_in);
+  //return ;
+};
+
+//console.log(cryptoHash("crccrecerf"));
+
+module.exports = cryptoHash;
